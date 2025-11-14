5a1e92c39707c5550ca8745fc975908c3b9b92ba
lev <lev19825@mail.ru>
2025-11-14 19:57:30 +0300

    Отскок

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.app.Converter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Converter converter = new Converter();
+        converter.run();
+    }
+}
